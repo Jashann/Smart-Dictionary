822d45a822dfa860637d56dd88e9d9025784563b
Jashann <48613250+Jashann@users.noreply.github.com>
2020-05-02 11:47:05 -0700

    Bookmark deleting functionality fixed!!

--- script.js
+++ script.js
@@ -58,14 +58,28 @@ const LocalStorage = (function(){
 
     function bookmarkDelete(nummbers){
         let array2d = JSON.parse(localStorage.getItem("bookmarked"));// [["desire", "to long or hope for : exhibit or feel desire for"],]
+        let newArray2d = [];
 
         nummbers.forEach(function(number,index){
-            if(index===0)
-                array2d.splice(number-1, number);
-            else
-                array2d.splice(number-1, number-1);
+            newArray2d.push(array2d.slice(number-1, number)) ;
         });
-        localStorage.setItem("bookmarked", JSON.stringify(array2d) )
+
+        array2d = array2d.filter(function(arr1){
+            let toBeDeleted = false;
+
+            // arr1 = ["desire", "to long or hope for : exhibit or feel desire for"]... till all present words
+            // arr2 = ["crave", " to ask for earnestly : beg, demand "], those which are to be deleted
+
+            newArray2d.forEach(function(arr2){ 
+                
+                if(arr1[0] === arr2[0][0]){
+                    toBeDeleted = true;
+                }
+            })
+            if(!toBeDeleted)
+                return arr1;
+        });
+        localStorage.setItem("bookmarked", JSON.stringify(array2d) );
     }
 
     //Public functions
