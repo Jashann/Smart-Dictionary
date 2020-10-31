bac4cef4a6b3e584ceecca259efc0085f9000de1
Jashann <48613250+Jashann@users.noreply.github.com>
2020-10-31 19:01:11 +0530

    Folder structure improved

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 requirejs.config({
   //By default load any module IDs from js/lib
-  baseUrl: 'Resources/App',
+  baseUrl: 'Resources/Javascript/App',
   //except, if the module ID starts with "app",
   //load it from the js/app directory. paths
   //config is relative to the baseUrl, and
